c68488925d9d56f20d1264bad7e78140d14c0564
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2024-08-13 20:49:28 +0100

    takes in user input and displays "Correct" or "Wrong". Gonna try add fade in animation for that text and fine tune visuals

--- app/src/main/java/com/example/numberguessing/MainActivity.kt
+++ app/src/main/java/com/example/numberguessing/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.numberguessing
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,12 +24,22 @@ class MainActivity : AppCompatActivity() {
         val guessBtn = findViewById<Button>(R.id.guessBtn)
         val numberInput = findViewById<TextInputEditText>(R.id.numberInput)
         val randomNumber = findViewById<TextView>(R.id.randomNumber)
-        var correctWrong = findViewById<TextView>(R.id.CorrectOrWrong)
+        val correctWrong = findViewById<TextView>(R.id.CorrectOrWrong)
+        correctWrong.isVisible = false  // initially invisible
 
         guessBtn.setOnClickListener{
-            var randNum = Random.nextInt(1, 100)
+            val randNum = Random.nextInt(1, 3)
 
             randomNumber.text = randNum.toString()
+            val userInput = numberInput.text.toString()
+
+            if (userInput == randNum.toString()){
+                correctWrong.text = "Correct!"
+                correctWrong.isVisible = true       // becomes visible
+            } else {
+                correctWrong.text = "Wrong!"
+                correctWrong.isVisible = true
+            }
         }
         }
     }
